5c897696b9c94835809cba6d7c4e295e165bcd33
Stephania Cavadas <stephaniac@interknowlogy.com>
2019-03-21 16:04:56 -0700

    Added globals changes

--- Unity/SmartHotelMR/Assets/SmartHotelMR/Scripts/Globals.cs
+++ Unity/SmartHotelMR/Assets/SmartHotelMR/Scripts/Globals.cs
@@ -1,14 +1,19 @@
-﻿namespace SmartHotelMR
+﻿using System;
+
+namespace SmartHotelMR
 {
-    public static class Globals
-    {
-        //Base URL of SmartHotelMR Service
-        public static readonly string ServiceBaseUrl = "";
+	public static class Globals
+	{
+		//Base URL of SmartHotelMR Service
+		private static readonly string _serviceBaseUrl = "";
+		public static string ServiceBaseUrl
+		{
+			get { return new Uri( new Uri( _serviceBaseUrl ), "/v1" ).ToString(); }
+		}
+		//API Key required to talk to SmartHotelMR Service
+		public static readonly string ApiKey = "";
 
-        //API Key required to talk to SmartHotelMR Service
-        public static readonly string ApiKey = "";
-
-        //Azure Spatial Anchors Account ID
-        public static readonly string SpatialAnchorsAccountId = "";
-    }
+		//Azure Spatial Anchors Account ID
+		public static readonly string SpatialAnchorsAccountId = "";
+	}
 }
